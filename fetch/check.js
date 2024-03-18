bba1681419b117005e54f958951402ba34e89fda
guiyi <1677852098@qq.com>
2024-03-18 16:19:00 +0800

    init

---
+++ fetch/check.js
@@ -0,0 +1,25 @@
+fetch("http://172.31.73.236:5520/user/check", {
+  "headers": {
+    "accept": "application/json, text/plain, */*",
+    "accept-language": "zh-CN,zh;q=0.9",
+    "content-type": "multipart/form-data; boundary=----WebKitFormBoundaryLrL2AvkBm1XKxgfs"
+  },
+  "referrerPolicy": "no-referrer",
+  "body": "------WebKitFormBoundaryLrL2AvkBm1XKxgfs\r\nContent-Disposition: form-data; name=\"userId\"\r\n\r\n2021150047\r\n------WebKitFormBoundaryLrL2AvkBm1XKxgfs\r\nContent-Disposition: form-data; name=\"password\"\r\n\r\n6be10c0967e0a674f6469e844498c328\r\n------WebKitFormBoundaryLrL2AvkBm1XKxgfs--\r\n",
+  "method": "POST",
+  "mode": "cors",
+  "credentials": "omit"
+});
+
+fetch("http://172.31.73.236:5520/user/check", {
+  "headers": {
+    "accept": "application/json, text/plain, */*",
+    "accept-language": "zh-CN,zh;q=0.9",
+    "content-type": "multipart/form-data; boundary=----WebKitFormBoundaryswPAlq6zdoIrAxIP"
+  },
+  "referrerPolicy": "no-referrer",
+  "body": "------WebKitFormBoundaryswPAlq6zdoIrAxIP\r\nContent-Disposition: form-data; name=\"userId\"\r\n\r\n2021150047\r\n------WebKitFormBoundaryswPAlq6zdoIrAxIP\r\nContent-Disposition: form-data; name=\"password\"\r\n\r\n6be10c0967e0a674f6469e844498c328\r\n------WebKitFormBoundaryswPAlq6zdoIrAxIP--\r\n",
+  "method": "POST",
+  "mode": "cors",
+  "credentials": "omit"
+});
---
+++ fetch/post.js
@@ -0,0 +1,38 @@
+fetch("http://172.31.73.236:5520/answer/post", {
+  "headers": {
+    "accept": "application/json, text/plain, */*",
+    "accept-language": "zh-CN,zh;q=0.9",
+    "content-type": "multipart/form-data; boundary=----WebKitFormBoundaryySIBdE7BR08x4U8A"
+  },
+  "referrerPolicy": "no-referrer",
+  "body": "------WebKitFormBoundaryySIBdE7BR08x4U8A\r\nContent-Disposition: form-data; name=\"userId\"\r\n\r\n2021150047\r\n------WebKitFormBoundaryySIBdE7BR08x4U8A\r\nContent-Disposition: form-data; name=\"questionId\"\r\n\r\n4\r\n------WebKitFormBoundaryySIBdE7BR08x4U8A\r\nContent-Disposition: form-data; name=\"content\"\r\n\r\n<!DOCTYPE html>\r\n<html lang=\"zh\">\r\n<head>\r\n  <meta charset=\"UTF-8\">\r\n  <title>apple网站</title>\r\n  <style>\r\n    h2 {\r\n      text-align: center;\r\n    }\r\n\r\n    hr {\r\n      height: 3px;\r\n      background-color: #5BFF24;\r\n    }\r\n\r\n    #div1 {\r\n      text-align: center;\r\n      margin: 20px;\r\n    }\r\n\r\n    a {\r\n      margin: 50px;\r\n      /*去掉下划线*/\r\n      text-decoration: none;\r\n    }\r\n  </style>\r\n</head>\r\n<body>\r\n<h2>apple网站</h2>\r\n<hr>\r\n<div id=\"div1\">\r\n  <a href=\"https://www.apple.com.cn/iphone\">\r\n    <img src=\"http://172.31.73.236/resource/img/exp4_1.jpg\" alt=\"error\">\r\n  </a>\r\n  <a href=\"https://www.apple.com.cn/iphone\">\r\n    <img src=\"http://172.31.73.236/resource/img/exp4_4.jpg\" alt=\"error\">\r\n  </a>\r\n  <a href=\"https://www.apple.com.cn/macbook-pro\">\r\n    <img src=\"http://172.31.73.236/resource/img/exp4_3.jpg\" alt=\"error\">\r\n  </a>\r\n  <a href=\"https://www.apple.com.cn/supplierresponsibility\">\r\n    <img src=\"http://172.31.73.236/resource/img/exp4_2.jpg\" alt=\"error\">\r\n  </a>\r\n</div>\r\n<hr>\r\n</body>\r\n</html>\r\n\r\n------WebKitFormBoundaryySIBdE7BR08x4U8A--\r\n",
+  "method": "POST",
+  "mode": "cors",
+  "credentials": "omit"
+});
+
+fetch("http://172.31.73.236:5520/answer/post", {
+  "headers": {
+    "accept": "application/json, text/plain, */*",
+    "accept-language": "zh-CN,zh;q=0.9",
+    "content-type": "multipart/form-data; boundary=----WebKitFormBoundarypYa9J3uzpVdQB5uD"
+  },
+  "referrerPolicy": "no-referrer",
+  "body": "------WebKitFormBoundarypYa9J3uzpVdQB5uD\r\nContent-Disposition: form-data; name=\"userId\"\r\n\r\n2021150047\r\n------WebKitFormBoundarypYa9J3uzpVdQB5uD\r\nContent-Disposition: form-data; name=\"questionId\"\r\n\r\n4\r\n------WebKitFormBoundarypYa9J3uzpVdQB5uD\r\nContent-Disposition: form-data; name=\"content\"\r\n\r\n<!DOCTYPE html>\r\n<html lang=\"zh\">\r\n<head>\r\n  <meta charset=\"UTF-8\">\r\n  <title>apple网站</title>\r\n  <style>\r\n    h2 {\r\n      text-align: center;\r\n    }\r\n\r\n    hr {\r\n      height: 3px;\r\n      background-color: #5BFF24;\r\n    }\r\n\r\n    #div1 {\r\n      text-align: center;\r\n      margin: 20px;\r\n    }\r\n\r\n    a {\r\n      margin: 50px;\r\n      /*去掉下划线*/\r\n      text-decoration: none;\r\n    }\r\n  </style>\r\n</head>\r\n<body>\r\n<h2>apple网站</h2>\r\n<hr>\r\n<div id=\"div1\">\r\n  <a href=\"https://www.apple.com.cn/iphone\">\r\n    <img src=\"http://172.31.73.236/resource/img/exp4_1.jpg\" alt=\"error\">\r\n  </a>\r\n  <a href=\"https://www.apple.com.cn/iphone\">\r\n    <img src=\"http://172.31.73.236/resource/img/exp4_4.jpg\" alt=\"error\">\r\n  </a>\r\n  <a href=\"https://www.apple.com.cn/macbook-pro\">\r\n    <img src=\"http://172.31.73.236/resource/img/exp4_3.jpg\" alt=\"error\">\r\n  </a>\r\n  <a href=\"https://www.apple.com.cn/supplierresponsibility\">\r\n    <img src=\"http://172.31.73.236/resource/img/exp4_2.jpg\" alt=\"error\">\r\n  </a>\r\n</div>\r\n<hr>\r\n</body>\r\n</html>\r\n\r\n------WebKitFormBoundarypYa9J3uzpVdQB5uD--\r\n",
+  "method": "POST",
+  "mode": "cors",
+  "credentials": "omit"
+});
+
+fetch("http://172.31.73.236:5520/answer/post", {
+  "headers": {
+    "accept": "application/json, text/plain, */*",
+    "accept-language": "zh-CN,zh;q=0.9",
+    "content-type": "multipart/form-data; boundary=----WebKitFormBoundaryFnxGmlHS08v9L7fC"
+  },
+  "referrerPolicy": "no-referrer",
+  "body": "------WebKitFormBoundaryFnxGmlHS08v9L7fC\r\nContent-Disposition: form-data; name=\"userId\"\r\n\r\n2021150047\r\n------WebKitFormBoundaryFnxGmlHS08v9L7fC\r\nContent-Disposition: form-data; name=\"questionId\"\r\n\r\n6\r\n------WebKitFormBoundaryFnxGmlHS08v9L7fC\r\nContent-Disposition: form-data; name=\"content\"\r\n\r\n<!DOCTYPE html>\r\n<html lang=\"zh\">\r\n<head>\r\n  <meta charset=\"UTF-8\">\r\n  <title>apple网站</title>\r\n  <style>\r\n    h2 {\r\n      text-align: center;\r\n    }\r\n\r\n    hr {\r\n      height: 3px;\r\n      background-color: #5BFF24;\r\n    }\r\n\r\n    #div1 {\r\n      text-align: center;\r\n      margin: 20px;\r\n    }\r\n\r\n    a {\r\n      margin: 50px;\r\n      /*去掉下划线*/\r\n      text-decoration: none;\r\n    }\r\n  </style>\r\n</head>\r\n<body>\r\n<h2>apple网站</h2>\r\n<hr>\r\n<div id=\"div1\">\r\n  <a href=\"https://www.apple.com.cn/iphone\">\r\n    <img src=\"http://172.31.73.236/resource/img/exp4_1.jpg\" alt=\"error\">\r\n  </a>\r\n  <a href=\"https://www.apple.com.cn/iphone\">\r\n    <img src=\"http://172.31.73.236/resource/img/exp4_4.jpg\" alt=\"error\">\r\n  </a>\r\n  <a href=\"https://www.apple.com.cn/macbook-pro\">\r\n    <img src=\"http://172.31.73.236/resource/img/exp4_3.jpg\" alt=\"error\">\r\n  </a>\r\n  <a href=\"https://www.apple.com.cn/supplierresponsibility\">\r\n    <img src=\"http://172.31.73.236/resource/img/exp4_2.jpg\" alt=\"error\">\r\n  </a>\r\n</div>\r\n<hr>\r\n</body>\r\n</html>\r\n\r\n------WebKitFormBoundaryFnxGmlHS08v9L7fC--\r\n",
+  "method": "POST",
+  "mode": "cors",
+  "credentials": "omit"
+});
